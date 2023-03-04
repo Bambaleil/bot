86633134be5da4a7011ada7b2ef043eb5c892649
Maxim Platonov <bliamba2000@gmail.com>
2023-03-04 23:55:28 +0400

    Я придумал только так сделать, потому что не понял как сделать caption только для одной фотографии.

--- handlers/default_heandlers/result.py
+++ handlers/default_heandlers/result.py
@@ -10,20 +10,12 @@ def result(message: Message, info_hotels: list) -> None:
     bot.set_state(user_id=message.chat.id, state=None, chat_id=message.chat.id)
     for hotel in info_hotels:
         if hotel.get('url'):
-            media = [InputMediaPhoto(url, caption=f"{hotel['name']}\n"
-                                                  f"Адрес: {hotel['adders']}\n"
-                                                  f"От центра: {hotel['by_center']} км.\n"
-                                                  f"Цена за ночь: {hotel['price']} USD.\n"
-                                                  f"Цена за весь период проживания: "
-                                                  f"{round(hotel['price'] * hotel['day_live'], 2)} USD.\n"
-                                                  f"'https://www.hotels.com/h{hotel['id']}.Hotel-Information'\n"
-                                     ) for url in hotel['url']]
+            media = [InputMediaPhoto(url) for url in hotel['url']]
             bot.send_media_group(message.chat.id, media)
-        else:
-            bot.send_message(message.chat.id, f"{hotel['name']}\n"
-                                              f"Адрес: {hotel['adders']}\n"
-                                              f"От центра: {hotel['by_center']} км.\n"
-                                              f"Цена за ночь: {hotel['price']} USD.\n"
-                                              f"Цена за весь период проживания: "
-                                              f"{hotel['price'] * hotel['day_live']} USD.\n"
-                                              f"'https://www.hotels.com/h{hotel['id']}.Hotel-Information'")
+        bot.send_message(message.chat.id, f"{hotel['name']}\n"
+                                          f"Адрес: {hotel['adders']}\n"
+                                          f"От центра: {hotel['by_center']} км.\n"
+                                          f"Цена за ночь: {hotel['price']} USD.\n"
+                                          f"Цена за весь период проживания: "
+                                          f"{hotel['price'] * hotel['day_live']} USD.\n"
+                                          f"'https://www.hotels.com/h{hotel['id']}.Hotel-Information'")
