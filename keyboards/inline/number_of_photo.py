b5cc51a8add024778852abb4fa0a60b1a875ae3d
Maxim Platonov <bliamba2000@gmail.com>
2023-06-26 20:54:00 +0400

    Апдейт

--- keyboards/inline/number_of_photo.py
+++ keyboards/inline/number_of_photo.py
@@ -10,12 +10,10 @@ from states.state_user_hotel import UserInfoState
 def number_buttons_p() -> InlineKeyboardMarkup:
     """ Клавиатура выбора количества фото, максимум 5 """
     key_bord = InlineKeyboardMarkup(row_width=5)
-    bth_1 = InlineKeyboardButton('1', callback_data='button_p_1')
-    bth_2 = InlineKeyboardButton('2', callback_data='button_p_2')
-    bth_3 = InlineKeyboardButton('3', callback_data='button_p_3')
-    bth_4 = InlineKeyboardButton('4', callback_data='button_p_4')
-    bth_5 = InlineKeyboardButton('5', callback_data='button_p_5')
-    key_bord.add(bth_1, bth_2, bth_3, bth_4, bth_5)
+    button = [
+        InlineKeyboardButton(str(num), callback_data=f'button_p_{num}') for num in range(1, 6)
+    ]
+    key_bord.add(*button)
     return key_bord
 
 
@@ -32,28 +30,21 @@ def process_callback_kb1btn1(call: types.CallbackQuery):
         chat_id=call.message.chat.id
     )
 
-    if code == '1':
-        logger.info('Пользователь нажал кнопку 1 для фото.')
-        num = 1
-        bot.send_message(call.from_user.id, text='Вы выбрали одно фото.')
-    elif code == '2':
-        logger.info('Пользователь нажал кнопку 2 для фото.')
-        num = 2
-        bot.send_message(call.from_user.id, text='Вы выбрали два фото.')
-    elif code == '3':
-        logger.info('Пользователь нажал кнопку 3 для фото.')
-        num = 3
-        bot.send_message(call.from_user.id, text='Вы выбрали три фото.')
-    elif code == '4':
-        logger.info('Пользователь нажал кнопку 4 для фото.')
-        num = 4
-        bot.send_message(call.from_user.id, text='Вы выбрали четыре фото.')
-    elif code == '5':
-        logger.info('Пользователь нажал кнопку 5 для фото.')
-        num = 5
-        bot.send_message(call.from_user.id, text='Вы выбрали пять фото.')
+    num_map = {
+        '1': 'одно',
+        '2': 'два',
+        '3': 'три',
+        '4': 'четыре',
+        '5': 'пять'
+    }
+
+    num = int(code)
+    logger.info(f"Пользователь нажал кнопку {num} для фото.")
+    bot.send_message(call.from_user.id, text=f'Вы выбрали {num_map[code]} фото.')
+
     with bot.retrieve_data(call.message.chat.id) as data:
         data['num_photo'] = num
+
     bot.send_message(
         chat_id=call.message.chat.id,
         text='Введите количество отелей не больше 10.',
